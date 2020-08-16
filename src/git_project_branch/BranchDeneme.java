e65f7d3312ec3d1f9c4d8305ba00a2eb7850c4b1
AlperenOz <alperenoz6001@gmail.com>
2020-08-16 12:38:44 -0400

    feature dan commit

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		System.out.println("16:28 -master branch ten ilk commit");
 
+		System.out.println("18:36 -feature branch ten ilk commit");
 	}
 
 }
